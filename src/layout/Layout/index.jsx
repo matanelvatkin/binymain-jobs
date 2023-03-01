43a6926bb65606b4c1b06ad88e67cd441c82182a
Yair Ken <yair.k.ken@gmail.com>
2023-03-01 15:17:36 +0200

    open layout

---
+++ src/layout/Layout/index.jsx
@@ -0,0 +1,14 @@
+import Header from "../Header"
+import Main from "../Main"
+
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    {/* <popUp/> */}
+    </>
+  )
+}
+
+export default Layout
