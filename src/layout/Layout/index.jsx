dd842f0684421e28a46fe9bfc0aba2f1fae89a6f
Yair Ken <yair.k.ken@gmail.com>
2023-03-01 21:34:16 +0200

    routes in main

--- src/layout/Layout/index.jsx
+++ src/layout/Layout/index.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react"
+import ContextFakeData from "../../context/fackData"
 import Header from "../Header"
 import Main from "../Main"
 
 function Layout() {
+
+const [setFakeData,fakeData]=useState("bla bla")
+
   return (
     <>
-    <Header/>
-    <Main/>
+      <Header/>
+    <ContextFakeData.Provider value={{fakeData}}>
+      <Main/>
+    </ContextFakeData.Provider>
     {/* <popUp/> */}
     </>
   )
