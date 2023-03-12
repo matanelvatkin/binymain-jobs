fc9e413dbe0f5005c19bb20aab9c3e4e22a20867
Hadar Naiman <hadarni@gmail.com>
2023-03-12 10:27:10 +0200

    add fuction of api calls

--- src/function/apiCalls.js
+++ src/function/apiCalls.js
@@ -1,8 +1,24 @@
+import axios from "axios";
 
+const apiCalls = async (method, port, url, information = null, param = null, header = null) => {
+    const fullUrl = `http://localhost:${port}/api/${url}`
 
-function apiCalls() {
-
-
+    try {
+        const data =
+            await axios({
+                method: method,
+                url: fullUrl,
+                data: information,
+                params: { q: param },
+                Headers: { header }
+            });
+        console.log(data.data)
+        return data.data;
+    }
+    catch (error) {
+        console.log(error)
+        throw error;
+    }
 }
 
-default export apiCalls;
+export default apiCalls;
