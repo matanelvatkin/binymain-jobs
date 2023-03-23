7802d26662730a457796a8d0c91a054b2ae54a05
yisrael <Yisrael94@gmail.com>
2023-03-23 11:44:41 +0200

    small styling changes

--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import styles from './style.module.css'
 import SelectIcon from '../../components/SelectIcon'
+import { MdFastfood } from 'react-icons/md'
 
-const categories = ["כיף", "מוסיקה", "יצירה", "אוכל", "הופעות", "טיולים",]
+const categories = [
+  "כיף",
+  "מוסיקה",
+  "יצירה",
+  "אוכל",
+  "הופעות",
+  "טיולים",
+]
 function Test() {
-    
+
   return (
     <div className={styles.main}>
-        Test
-        <SelectIcon array={categories} />
+      Test
+      <SelectIcon array={categories} icon={<MdFastfood/>}/>
     </div>
   )
 }
