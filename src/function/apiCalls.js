c0deb3ec8ac0ee298c4bfe3355b2d0b4e68be94a
kobikru <75983501+kobikru@users.noreply.github.com>
2023-04-19 11:10:33 +0300

    before matanel

--- src/function/apiCalls.js
+++ src/function/apiCalls.js
@@ -9,7 +9,9 @@ const apiCalls = async (
   param = null,
   header = null
 ) => {
-  const baseUrl = process.env.REACT_APP_BASE_URL || `http://localhost:${PORT}`;
+  const baseUrl = `http://localhost:${PORT}`;
+  //  process.env.REACT_APP_BASE_URL ||
+
   const fullUrl = `${baseUrl}/api${url}`;
   try {
     const data = await axios({
