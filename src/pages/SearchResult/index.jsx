7b17bc6eb1c78c638f23041146108775cd48e1f9
ShaharMarom2 <106477834+ShaharMarom2@users.noreply.github.com>
2023-05-01 10:17:31 +0300

    search fixed

--- src/pages/SearchResult/index.jsx
+++ src/pages/SearchResult/index.jsx
@@ -1,53 +1,73 @@
-import { useParams } from 'react-router-dom'
-import EventCard from '../../components/EventCard'
-import { useState, useEffect } from 'react'
-import apiCalls from '../../function/apiCalls'
-import Loader from '../../components/Loader'
-import EmptySearch from '../../components/EmptySearch'
-import InvalidQuery from '../../components/InvalidQuery'
-
+import { useParams } from "react-router-dom";
+import EventCard from "../../components/EventCard";
+import { useState, useEffect } from "react";
+import apiCalls from "../../function/apiCalls";
+import Loader from "../../components/Loader";
+import EmptySearch from "../../components/EmptySearch";
+import InvalidQuery from "../../components/InvalidQuery";
+import style from "./style.module.css";
 
 export default function SearchResult() {
-  
-  const [isLoading, setIsLoading] = useState(true)
-  const [isInvalidQuery, setIsInvalidQuery] = useState(false)
-  const [events, setEvents] = useState()
+  const [isLoading, setIsLoading] = useState(true);
+  const [isInvalidQuery, setIsInvalidQuery] = useState(false);
+  const [events, setEvents] = useState();
 
-  let { query } = useParams()
-  let filter = JSON.parse(decodeURIComponent(query))
+  let { query } = useParams();
+  let filter = JSON.parse(decodeURIComponent(query));
 
-  console.log(filter);
-  
+  // console.log(filter);
+
+  let singleEventsFilter = {
+    ...filter,
+    $and: [...filter.$and, { date: { $not: { $type: "array" } } }],
+  };
+
+  let repetedEventsFilter = {
+    ...filter,
+    $and: [
+      {
+        date: {
+          $elemMatch: { $gte: filter.$and[0].$gte, $lte: filter.$and[1].$lte },
+        },
+      },
+      { date: { $type: "array" } },
+    ],
+  };
+
   async function fetchEvents() {
-    let apiEvents = await apiCalls('post', '/event', filter)
-    setEvents(() => apiEvents)
+    let apiSingleEvents = await apiCalls("post", "/event", singleEventsFilter);
+    let apiReptedEvents = await apiCalls("post", "/event", repetedEventsFilter);
+    let apiEvents = apiSingleEvents.concat(apiReptedEvents);
+    console.log(apiEvents);
+    setEvents(() => apiEvents);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     try {
-      fetchEvents()
-    }catch(err) {
+      fetchEvents();
+    } catch (err) {
       console.log(err);
-      setIsInvalidQuery(() => true)
+      setIsInvalidQuery(() => true);
     }
-  },[])
+  }, []);
 
-  useEffect(()=>{
-    if(Array.isArray(events))
-      setIsLoading(() => false) 
-  },[events])
+  useEffect(() => {
+    if (Array.isArray(events)) setIsLoading(() => false);
+  }, [events]);
 
   return (
-    <div>
-      {
-        !isInvalidQuery ?
-          isLoading ? 
-            <Loader /> :
-            !events ? 
-              <EmptySearch /> :
-              <EventCard events={events} /> : 
-          <InvalidQuery />
-      }
+    <div className={style.container}>
+      {!isInvalidQuery ? (
+        isLoading ? (
+          <Loader />
+        ) : !events ? (
+          <EmptySearch />
+        ) : (
+          <EventCard events={events} />
+        )
+      ) : (
+        <InvalidQuery />
+      )}
     </div>
-  )
+  );
 }
