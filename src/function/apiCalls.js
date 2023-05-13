fbcf3724b75a7a3a9ab0fae087209941743840b3
hadarna <hadarni@gmail.com>
2023-05-13 22:25:58 +0300

    UI changes

--- src/function/apiCalls.js
+++ src/function/apiCalls.js
@@ -9,7 +9,7 @@ const apiCalls = async (
   param = null,
   header = null
 ) => {
-  const baseUrl = process.env.REACT_APP_BASE_URL || `http://localhost:5000`;
+  const baseUrl = "https://server-production-7ef9.up.railway.app"
 
   const fullUrl = `${baseUrl}/api/${url}`;
   try {
@@ -20,6 +20,7 @@ const apiCalls = async (
       params: { q: param },
       headers: { header },
     });
+    console.log("******************", data.data);
     return data.data;
   } catch (error) {
     console.log(error);
