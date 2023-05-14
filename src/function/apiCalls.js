83b212f6b239b100326f776d88e40a1368a2d28d
binyamintech <82864821+binyamintech@users.noreply.github.com>
2023-05-14 12:49:22 +0300

    Update apiCalls.js

--- src/function/apiCalls.js
+++ src/function/apiCalls.js
@@ -10,7 +10,8 @@ const apiCalls = async (
   header = null
 ) => {
 
-  const baseUrl = "https://server-production-7ef9.up.railway.app"
+   const baseUrl = process.env.REACT_APP_BASE_URL || `http://localhost:5000`;
+//  const baseUrl = "https://server-production-7ef9.up.railway.app"
 
 
 
