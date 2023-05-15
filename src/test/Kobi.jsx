48b30879f7b0204ab580c3a043013a0d44417a68
kobikru <75983501+kobikru@users.noreply.github.com>
2023-05-15 15:19:31 +0300

    crop

--- src/test/Kobi.jsx
+++ src/test/Kobi.jsx
@@ -3,22 +3,12 @@ import Input from "../components/Input";
 import NewEvent from "../pages/NewEvent";
 import $ from "jquery";
 import apiCalls from "../function/apiCalls";
+import ImageUpload from "../components/ImageUpload/ImageUpload";
 
 export default function Kobi() {
-  const [myImage, setMyImage] = useState("");
-  apiCalls("post", "event").then((event) => {
-    setMyImage(event[159].cardImageURL);
-    console.log({ myImage });
-  });
-  const testImage = "http://localhost:5000/upload/Kobi_pic-03052023170005.jpeg";
-  console.log(testImage.toString());
-  useEffect(() => {
-    setMyImage(testImage);
-    console.log("22", testImage.toString());
-  }, []);
   return (
     <div>
-      <img src={testImage} alt="Event pic" />
+      <ImageUpload />
     </div>
   );
 }
