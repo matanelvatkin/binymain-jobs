361921ce6d895c59177b4e7700b17a08e78c1fe5
kobikru <75983501+kobikru@users.noreply.github.com>
2023-07-06 14:23:24 +0300

    accuratelocation

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,15 +1,15 @@
-import style from './style.module.css'
+import styles from "./style.module.css";
 export default function Loader() {
   return (
-    <div class={style.ldsroller}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className={styles.ldsroller}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
-  )
+  );
 }
