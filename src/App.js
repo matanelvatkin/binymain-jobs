d55f99ab92c857443274c77aed73b01f171e0d81
matanelvatkin <matanelvatkin@gmail.com>
2023-07-18 18:42:52 +0300

    add ga to login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter } from "react-router-dom";
 import Layout from "./layout/Layout";
 import MainTest from "./test/MainTest";
 import './app.css';
-
+import ReactGA from 'react-ga4';
+  
+const TRACKING_ID = "G-0KR78SJP2Q"; 
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
