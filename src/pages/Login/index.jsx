1082ea720354607095e6395ecdae9eaef47d375d
matanelvatkin <matanelvatkin@gmail.com>
2023-07-19 10:37:11 +0300

    send value to ga

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -56,7 +56,7 @@ function Login() {
       setUser(res.user);
       setToken(res.token);
       localStorage.setItem("Token", res.token);
-      eventTracker('login',JSON.stringify(res.user),'userLogin')
+      eventTracker('login',res.user.email,'userLogin')
       navigate("/");
     } else {
       alert(res);
