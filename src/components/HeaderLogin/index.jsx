cd952530efbd0e0b7077351fc8a0a715cbf791cb
kobikru <75983501+kobikru@users.noreply.github.com>
2023-07-20 18:13:54 +0300

    KorePo

--- src/components/HeaderLogin/index.jsx
+++ src/components/HeaderLogin/index.jsx
@@ -2,11 +2,25 @@ import React, { useContext } from "react";
 import styles from "./style.module.css";
 import { BiAnalyse } from "react-icons/bi";
 import Logo from "../Logo";
+import { Link } from "react-router-dom";
 
 function HeaderLogin() {
   return (
     <div className={styles.main}>
-      <img src={"/Frame 33508.svg"} alt="" className={styles.logo} />
+      <img src={"/KorePo.svg"} alt="" className={styles.logo} />
+      <Link
+        className={styles.declaimer}
+        to={`https://wa.me/+972525666679?text=היי לגבי האפליקציה KorePo רציתי להגיד ש`}
+      >
+        <img
+          src={"/uil_comment-message.svg"}
+          alt=""
+          className={styles.iconDeclaimer}
+        />
+        <span className={styles.declaimertext}>
+          אנחנו בהרצה נשמח לקבל ממך פידבק
+        </span>
+      </Link>
     </div>
   );
 }
