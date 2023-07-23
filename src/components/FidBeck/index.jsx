596daabbac014f2ee8869e3159a73d243fc6ce5e
Yair Ken <yair.k.ken@gmail.com>
2023-07-23 22:12:19 +0300

    fix fidbeck css

--- src/components/FidBeck/index.jsx
+++ src/components/FidBeck/index.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "./style.module.css";
+
 function FidBeck() {
   return (
-    <span>
-     {/* אנחנו בהרצה! נשמח לקבל ממך פידבק */}
+    <Link
+    className={styles.declaimer}
+    to={`https://wa.me/+972525666679?text=היי לגבי האפליקציה KorePo רציתי להגיד ש`}
+  >
+    <img
+      src={"/uil_comment-message.svg"}
+      alt=""
+      className={styles.iconDeclaimer}
+    />
+    <span className={styles.declaimertext}>
+      אנחנו בהרצה נשמח לקבל ממך פידבק
     </span>
+  </Link>
   );
 }
 
