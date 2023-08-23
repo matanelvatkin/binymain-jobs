e9f896ca3d96c630f308f61b9f4d2f4291fe90d6
kobikru <75983501+kobikru@users.noreply.github.com>
2023-08-23 21:01:29 +0300

    map and location search

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,31 +1,37 @@
 import styles from "./style.module.css";
-import React, { useCallback, useMemo, useState } from "react";
-import {
-  GoogleMap,
-  Marker,
-  useJsApiLoader,
-  useLoadScript,
-} from "@react-google-maps/api";
-
-const containerStyle = {
-  width: "400px",
-  height: "400px",
-};
+import { useState, useMemo, useEffect } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+import LocationSearchInput from "../LocationSearchInput/LocationSearchInput";
 
 export default function Map() {
-  const { isLoaded } = useLoadScript({
+  const google = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_API_GOOGLE_MAPS,
+    libraries: ["places"],
   });
-  if (!isLoaded) return <div>Loading...</div>;
+  if (!google.isLoaded) return <div>Loading...</div>;
   return <MapIn />;
 }
 
 function MapIn() {
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
-
+  const center = useMemo(() => ({ lat: 31.94864, lng: 35.2568 }), []);
+  const [selected, setSelected] = useState([]);
+  useEffect(() => {
+    console.log(selected);
+  }, [selected]);
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
-    </GoogleMap>
+    <>
+      <div className={styles.places_container}>
+        <LocationSearchInput setSelected={setSelected} />
+      </div>
+
+      <GoogleMap zoom={10} center={center} mapContainerClassName={styles.map}>
+        {selected.length > 0 &&
+          selected.map((mark) => <Marker position={mark} />)}
+      </GoogleMap>
+    </>
   );
 }
